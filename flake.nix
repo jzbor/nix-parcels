8122b288e098bcd03e4f8bf14bc20791576471e8
jzbor <jzbor@posteo.net>
2023-03-14 19:09:40 +0100

    Adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "jzbor's personal overlay";
+
+  outputs = { self, ... }: {
+    # Default overlay
+    overlay = import ./default.nix
+  };
+
+  # See also: https://github.com/mozilla/nixpkgs-mozilla
+}
