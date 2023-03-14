6f8b9a1e15fa50b0889974ede9bfae8320b0ff66
jzbor <jzbor@posteo.net>
2023-03-15 00:50:18 +0100

    Fixing nextcloud-client derivation

--- default.nix
+++ default.nix
@@ -3,6 +3,15 @@ self: super:
   adi1090x-plymouth = super.callPackage ./packages/adi1090x-plymouth {};
   buttermilk = super.callPackage ./packages/buttermilk {};
   marswm = super.callPackage ./packages/marswm {};
-  nextcloud-client = super.callPackage ./packages/nextcloud-client {};
   pademelon = super.callPackage ./packages/pademelon {};
+
+  nextcloud-client = super.nextcloud-client.overrideAttrs (old: rec {
+    version = "3.7.3";
+    src = super.fetchFromGitHub {
+      owner = "nextcloud";
+      repo = "desktop";
+      rev = "v${version}";
+      sha256 = "sha256-SzQdT2BJ0iIMTScJ7ft47oKd+na5MlOx5xRB1SQ7CBc=";
+    };
+  });
 }
