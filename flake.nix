9a984048d605ed764e2e847884bbdec05975f2c0
jzbor <jzbor@posteo.net>
2023-03-20 19:48:49 +0100

    Adding outputs based on overlay

--- flake.nix
+++ flake.nix
@@ -1,10 +1,23 @@
 {
   description = "jzbor's personal overlay";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, ... }: {
+  outputs = { self, nixpkgs, utils }:
+    let
+      flakePackages = system: import nixpkgs {
+        overlays = [ (import ./default.nix) ];
+        inherit system;
+      };
+    in utils.lib.eachDefaultSystem (system: rec {
+      legacyPackages = flakePackages system;
+      packages = utils.lib.flattenTree {
+        inherit legacyPackages;
+      };
+  }) // {
     # Default overlay
     overlay = import ./default.nix;
   };
-
-  # See also: https://github.com/mozilla/nixpkgs-mozilla
 }
