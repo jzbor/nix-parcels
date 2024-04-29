92ffc833363ce26df75985f819cd3f4663f64830
jzbor <jzbor@posteo.net>
2024-04-29 18:23:04 +0200

    Enable cgo for obligator

--- overlays/obligator/default.nix
+++ overlays/obligator/default.nix
@@ -17,6 +17,11 @@ self: super:
       };
       vendorHash = "sha256-snI3htyuBeeTOND8X5Wi+3WEW5xC+Y+C8jMfmiYAur4=";
 
+      # buildInputs = with pkgs; [ sqlite ];
+
+      # Enable CGO if SQLite support is required.
+      CGO_ENABLED = "1";
+
       meta = with lib; {
         description = "Simple and opinionated OpenID Connect server designed for self-hosters";
         homepage = "https://www.lastlogin.io/";
