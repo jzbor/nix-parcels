7ad56d5d0ca92a533b3861ac9a0b32f2890d14ac
jzbor <jzbor@posteo.net>
2024-09-23 16:09:55 +0200

    Updating erth-exporter

--- packages/erth-exporter/default.nix
+++ packages/erth-exporter/default.nix
@@ -1,19 +1,18 @@
-{ lib, craneLib, fetchFromGitHub, ... }:
+{ craneLib, fetchFromGitHub, ... }:
 
 craneLib.buildPackage rec {
   pname = "erth-exporter";
-  version = "0.3.2";
+  version = "0.3.3";
 
   src = craneLib.cleanCargoSource (fetchFromGitHub {
     owner = "jzbor";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-bXO0K57xt0Pik8yTYCIK8TAmheXOWgAM/ECWZ7d+SK8=";
+    sha256 = "sha256-Iwz/bsw4qbavYgvMqDEfqqC/H9ZMF+Dspl9zajzAh4I=";
   });
 
-  meta = with lib; {
+  meta = {
     description = "Prometheus erth-exporter";
     homepage = "https://github.com/jzbor/erth-exporter";
-    # license = licenses.mit;
   };
 }
