934b3d6a039b3f3651dab611bf05eeda91cd2887
jzbor <jzbor@posteo.net>
2024-10-24 14:24:52 +0200

    Adding X11 build of groff

--- packages/default.nix
+++ packages/default.nix
@@ -17,6 +17,7 @@ in {
   foliot = pkgs.callPackage ./foliot extraArgs;
   gama = pkgs.callPackage ./gama extraArgs;
   goatcounter = pkgs.callPackage ./goatcounter extraArgs;
+  groffX = pkgs.callPackage ./groffX extraArgs;
   kanagawa-gtk-theme = pkgs.callPackage ./kanagawa-gtk-theme extraArgs;
   lash = inputs.lash.packages.${system}.default;
   lisho = pkgs.callPackage ./lisho extraArgs;
