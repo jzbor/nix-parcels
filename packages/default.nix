a508ec9a4cca008a05f5664f613e4066c784271e
jzbor <jzbor@posteo.net>
2024-11-29 23:01:10 +0100

    Adding millet language server

--- packages/default.nix
+++ packages/default.nix
@@ -24,6 +24,7 @@ in {
   marswm = inputs.marswm.packages.${system}.default;
   inherit (inputs.marswm.packages.${system}) marswm-scripts;
   marmite = pkgs.callPackage ./marmite extraArgs;
+  millet = pkgs.callPackage ./millet extraArgs;
   neonmodem = pkgs.callPackage ./neonmodem extraArgs;
   nixpkgs-tracker = pkgs.callPackage ./nixpkgs-tracker extraArgs;
   obligator = pkgs.callPackage ./obligator extraArgs;
---
+++ packages/millet/default.nix
@@ -0,0 +1,21 @@
+{ lib, craneLib, fetchFromGitHub, ...  }:
+
+craneLib.buildPackage rec {
+  pname = "millet";
+  version = "0.14.7";
+
+  doCheck = false;
+
+  src = fetchFromGitHub {
+    owner = "azdavis";
+    repo = pname;
+    rev = "v" + version;
+    sha256 = "sha256-nj/ueMQSAqdcAG5jaSZBOHd4D4l7xD3i+LO9MUYsz0c=";
+  };
+
+  meta = with lib; {
+    description = "A language server for Standard ML.";
+    homepage = "https://github.com/azdavis/millet";
+    license = licenses.mit;
+  };
+}
