5c9e7a4972c09890ad2b2e9a4ca6d92d8d394721
jzbor <jzbor@posteo.net>
2025-02-17 17:51:57 +0100

    zizmor: 0.1.1 -> 0.3.1

--- packages/zizmor/default.nix
+++ packages/zizmor/default.nix
@@ -2,13 +2,13 @@
 
 craneLib.buildPackage rec {
   pname = "zizmor";
-  version = "0.1.1";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "woodruffw";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-IFo/sZyZT/oqVB1AS3MSJLbSDVFXKccyF4LU1DAKnEE=";
+    sha256 = "sha256-NydP0p8R7rNDZedsefKScVfSkqfZmvGjtvttph2BJK4=";
   };
 
   nativeBuildInputs = with pkgs; [
