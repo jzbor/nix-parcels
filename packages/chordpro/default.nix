33ddcc3cbe6453a453c80dd63a8bc70b60c450a0
jzbor <jzbor@posteo.net>
2025-02-17 17:17:34 +0100

    chordpro: 6.010 -> 6.070

--- packages/chordpro/default.nix
+++ packages/chordpro/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
-pkgs.perlPackages.buildPerlPackage {
+pkgs.perlPackages.buildPerlPackage rec {
   pname = "App-Music-ChordPro";
-  version = "6.010";
+  version = "6.070";
   src = pkgs.fetchurl {
-    url = "mirror://cpan/authors/id/J/JV/JV/App-Music-ChordPro-6.010.tar.gz";
-    hash = "sha256-SqTkbR2bWIMcU5gSRf2WW6s1ckHtJVPkxj/bBO9X4kM=";
+    url = "mirror://cpan/authors/id/J/JV/JV/App-Music-ChordPro-${version}.tar.gz";
+    hash = "sha256-j1YKVwkqJz8LAcmeHlh7kBaFGM4ZlyUHEHN5w0V6zwc=";
   };
   buildInputs = with pkgs.pkgs.perlPackages; [ PodParser ];
   propagatedBuildInputs = with pkgs.pkgs.perlPackages; [ AppPackager FileLoadLines IOString ImageInfo PDFAPI2 StringInterpolateNamed TextLayout ]
