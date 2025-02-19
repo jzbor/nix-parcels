ff2d9f5d2d23ef63e2ef7f6cc4e7079812fb040d
jzbor <jzbor@posteo.net>
2025-02-19 16:13:11 +0100

    Revert "obligator: 0.1.0 -> 0.3.0"
    This reverts commit 4d6024b3108ceef48ded1f0b7d9faed962696ae2.

--- packages/obligator/default.nix
+++ packages/obligator/default.nix
@@ -1,14 +1,14 @@
 { lib, buildGoModule, fetchFromGitHub, ... }:
 
-buildGoModule rec {
+buildGoModule {
   pname = "obligator";
-  version = "0.3.0";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "lastlogin-io";
     repo = "obligator";
-    rev = version;
-    hash = "sha256-4+9qUsyoBUphpVm0hDI0MYCJ1fUlBCAggcqVQJhILp0=";
+    rev = "37f75cc861f1bcd0dbf0f26a58e0f45bdae032ff";
+    hash = "sha256-Di4nH/veqp1dTwJBavRENOI/NMsvhNtgN0QZ4OU4lfw=";
   };
   vendorHash = "sha256-snI3htyuBeeTOND8X5Wi+3WEW5xC+Y+C8jMfmiYAur4=";
 
