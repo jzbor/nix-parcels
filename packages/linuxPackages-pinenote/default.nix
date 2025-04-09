9b79d76089b3f5cf6280e21335770f9c34cf5a8f
jzbor <jzbor@posteo.net>
2025-04-09 17:36:00 +0200

    linuxPackages_pinenote: Trying out a different setup

--- packages/linuxPackages-pinenote/default.nix
+++ packages/linuxPackages-pinenote/default.nix
@@ -1,24 +1,7 @@
 { pkgs, fetchFromGitHub, ... }:
 
-pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
-  argsOverride = rec {
-    src = fetchFromGitHub {
-      owner = "m-weigand";
-      repo = "linux";
-      rev = "branch_pinenote_6-12-11";
-      sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
-    };
-    version = "6.12.11";
-    modDirVersion = "6.12.11";
-    configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
-    structuredExtraConfig = with pkgs.lib.kernel; {
-      CRYPTO_AEGIS128_SIMD = no;
-    };
-  };
-})
-
 # pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
-#   argsOverride = {
+#   argsOverride = rec {
 #     src = fetchFromGitHub {
 #       owner = "m-weigand";
 #       repo = "linux";
@@ -27,10 +10,27 @@ pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
 #     };
 #     version = "6.12.11";
 #     modDirVersion = "6.12.11";
-#     defconfig = "pinenote_defconfig";
-#     ignoreConfigErrors = true;
+#     configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
+#     structuredExtraConfig = with pkgs.lib.kernel; {
+#       CRYPTO_AEGIS128_SIMD = no;
+#     };
 #   };
 # })
+
+pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
+  argsOverride = {
+    src = fetchFromGitHub {
+      owner = "m-weigand";
+      repo = "linux";
+      rev = "branch_pinenote_6-12-11";
+      sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
+    };
+    version = "6.12.11";
+    modDirVersion = "6.12.11";
+    defconfig = "pinenote_defconfig";
+    ignoreConfigErrors = true;
+  };
+})
 
 
 
