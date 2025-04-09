dffc92eea839c9e5d32043bff4df390da371200f
jzbor <jzbor@posteo.net>
2025-04-09 14:16:37 +0200

    linuxPackages-pinenote: Attempt at fixing build errors

--- packages/linuxPackages-pinenote/default.nix
+++ packages/linuxPackages-pinenote/default.nix
@@ -1,7 +1,24 @@
 { pkgs, fetchFromGitHub, ... }:
 
+pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
+  argsOverride = rec {
+    src = fetchFromGitHub {
+      owner = "m-weigand";
+      repo = "linux";
+      rev = "branch_pinenote_6-12-11";
+      sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
+    };
+    version = "6.12.11";
+    modDirVersion = "6.12.11";
+    configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
+    structuredExtraConfig = with pkgs.lib.kernel; {
+      CRYPTO_AEGIS128_SIMD = no;
+    };
+  };
+})
+
 # pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
-#   argsOverride = rec {
+#   argsOverride = {
 #     src = fetchFromGitHub {
 #       owner = "m-weigand";
 #       repo = "linux";
@@ -10,17 +27,24 @@
 #     };
 #     version = "6.12.11";
 #     modDirVersion = "6.12.11";
-#     configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
+#     defconfig = "pinenote_defconfig";
+#     ignoreConfigErrors = true;
 #   };
 # })
 
-pkgs.linuxPackages_custom rec {
-    src = fetchFromGitHub {
-      owner = "m-weigand";
-      repo = "linux";
-      rev = "branch_pinenote_6-12-11";
-      sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
-    };
-    version = "6.12.11";
-    configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
-}
+
+
+# pkgs.linuxPackages_custom rec {
+#     src = fetchFromGitHub {
+#       owner = "m-weigand";
+#       repo = "linux";
+#       rev = "branch_pinenote_6-12-11";
+#       sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
+#     };
+#     version = "6.12.11";
+#     configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
+#     extraConfig = ''
+#       # See comments on https://github.com/NixOS/nixpkgs/commit/9b67ea9106102d882f53d62890468071900b9647
+#       CRYPTO_AEGIS128_SIMD = no;
+#     '';
+# }
