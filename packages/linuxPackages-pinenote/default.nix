2f4d70094c213238672556b1f4d871fdcb1b952b
jzbor <jzbor@posteo.net>
2025-04-09 02:04:19 +0200

    linuxPackages-pinenote: use linuxPackages_custom

--- packages/linuxPackages-pinenote/default.nix
+++ packages/linuxPackages-pinenote/default.nix
@@ -1,15 +1,26 @@
 { pkgs, fetchFromGitHub, ... }:
 
-pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
-  argsOverride = rec {
+# pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_12.override {
+#   argsOverride = rec {
+#     src = fetchFromGitHub {
+#       owner = "m-weigand";
+#       repo = "linux";
+#       rev = "branch_pinenote_6-12-11";
+#       sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
+#     };
+#     version = "6.12.11";
+#     modDirVersion = "6.12.11";
+#     configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
+#   };
+# })
+
+pkgs.linuxPackages_custom rec {
     src = fetchFromGitHub {
       owner = "m-weigand";
       repo = "linux";
-      rev = "branch_pinenote_6-12-rc3_v1";
-      sha256 = "sha256-kjY63lyWoxeQC75UzoVcjDNrtooK1n8c1ILn9tdz81c=";
+      rev = "branch_pinenote_6-12-11";
+      sha256 = "sha256-jbx+BWUo2dNYMrUulqhiO9efkLPhJUs5XjvWooMSxJw=";
     };
-    version = "6.12.0-rc3";
-    modDirVersion = "6.12.0-rc3";
+    version = "6.12.11";
     configfile = src + /arch/arm64/configs/pinenote_defconfig;  # TODO vendor if it slows eval down too much
-  };
-})
+}
