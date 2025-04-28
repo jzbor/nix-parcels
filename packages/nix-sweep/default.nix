25875a177e1790d55d086fa9ddd4a43119054e2d
jzbor <jzbor@posteo.net>
2025-04-28 23:33:21 +0200

    nix-sweep: 0.2.1 -> 0.2.2

--- packages/nix-sweep/default.nix
+++ packages/nix-sweep/default.nix
@@ -2,17 +2,17 @@
 
 craneLib.buildPackage rec {
   pname = "nix-sweep";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "jzbor";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-WuIY9/wtBP6wcsi0fXra108tedU+SEy7zpLe3K8UbFc=";
+    sha256 = "sha256-ygx5FTL5A5FbG/c2WWH6AWdrYqwsLVL+9kar2Wz/0jo=";
   };
 
   meta = with lib; {
-    description = "Utility to clean up old Nix profile generations and gc roots";
+    description = "Utility to clean up old Nix profile generations and left-over garbage collection roots";
     homepage = "https://github.com/jzbor/nix-sweep";
     license = licenses.mit;
   };
