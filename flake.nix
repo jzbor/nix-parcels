8fcc1eac5830adb3b7eab69c5a3db5d63a014b10
jzbor <jzbor@posteo.net>
2025-06-19 22:59:06 +0200

    Make cf nixpkgs follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,7 +6,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-chordpro.url = "github:nixos/nixpkgs/658e7223191d2598641d50ee4e898126768fe847";
 
-    cf.url = "github:jzbor/cornflakes";
+    cf = {
+      url = "github:jzbor/cornflakes";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     crane.url = "github:ipetkov/crane";
 
     rust-overlay = {
